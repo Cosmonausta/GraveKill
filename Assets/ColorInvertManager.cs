aafea3c0cb186e9dad1d66a8d4b19316136708e2
Joshua Ásberg O'Neill <joshoneill94@gmail.com>
2018-07-12 21:28:39 +0000

    Init Commit
    This time it's going to work

---
+++ Assets/ColorInvertManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorInvertManager : MonoBehaviour {
+
+    private GameObject player;
+    private List<GameObject> enemies;
+
+    private void Awake()
+    {
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Vector2 moveDirection;
+    private Vector2 moveVelocity;
+    private Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        moveVelocity = moveDirection * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
+}
