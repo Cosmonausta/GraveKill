41cc9e0a329d8ab99d7d91cb0c14c818ac090ad5
HjorturJ <hjorturv17@ru.is>
2018-07-14 00:01:31 +0000

    Player animations and rotation
    Imported main character sprite sheet and implemented player rotation towards mouse position. Also set up animation connections in code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float timeBetweenScytheShines = 5;
 
+    private float idleTimer;
     private Vector2 moveDirection;
     private Vector2 moveVelocity;
     private Rigidbody2D rb;
-
+    private Animator animator;
+    private bool currentlyMoving = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -19,10 +26,46 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
         moveVelocity = moveDirection * moveSpeed;
+
+        currentlyMoving = SetMovingAnimation();
+
+        PlayScytheShineAnimationOnTimer();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = moveVelocity;
+    }
+
+    private void PlayScytheShineAnimationOnTimer()
+    {
+        if(!currentlyMoving)
+        {
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= timeBetweenScytheShines)
+            {
+                animator.Play("ScytheShine");
+                idleTimer = 0f;
+            }
+        }
+        else
+        {
+            idleTimer = 0f;
+        }
+    }
+
+    private bool SetMovingAnimation()
+    {
+        if (rb.velocity != Vector2.zero)
+        {
+            animator.SetBool("isMoving", true);
+            return true;
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+            return false;
+        }
     }
 }
